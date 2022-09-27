e113b5dc86a010d5b9c45a67c603e0d86835c778
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2022-09-27 12:09:10 +0000

    Create reverseAnArray.py

---
+++ reverseAnArray.py
@@ -0,0 +1,19 @@
+'''
+
+Reverse an array of integers.
+
+'''
+
+#______________________________________
+'''
+My Soln
+'''
+
+def reverseArray(a):
+    # Write your code here
+    return a[::-1]
+
+#______________________________________
+'''
+
+'''
