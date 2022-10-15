3e1600738f406b5b13059a7c6f6beda946a01e7a
dacostafrankaboagye <60321537+dacostafrankaboagye@users.noreply.github.com>
2022-10-15 22:58:23 +0000

    Update LinkedListAdd Two Numbers.py

--- LinkedListAdd Two Numbers.py
+++ LinkedListAdd Two Numbers.py
@@ -1,5 +1,13 @@
 
 
+
+'''
+You are given two non-empty linked lists representing two non-negative integers. 
+The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
+
+You may assume the two numbers do not contain any leading zero, except the number 0 itself.
+'''
+
 ans = ""
 def rec(l1):
     global ans
@@ -9,8 +17,10 @@ def rec(l1):
     else:
         ans += str(l1.val)
         l1 = l1.next
-        
         return rec(l1)
+
+
+
 class Solution:
 
 
@@ -19,20 +29,28 @@ class Solution:
         pointer = result
 
         global ans
-        sum = 0
+        sum1 = ""
+        sum2 = ""
 
         rec(l1)
-        sum += int(ans)
+        sum1 = ans 
         ans = ""
-        rec(l2)
-        sum += int(ans)
-        ans = ""
-        sum = str(sum)
-        sum = sum[::-1]
-        print(sum)
+        print(sum1)
 
-        for i in range(len(sum)):
-            data = sum[i]
+        rec(l2)
+        sum2 = ans
+        ans = ""
+        print(sum2)
+
+        sum1 = sum1[::-1]
+        sum2 = sum2[::-1]
+        summ = int(sum1) + int(sum2)
+        summ = str(summ)
+        summ = summ[::-1]
+
+
+        for i in range(len(summ)):
+            data = summ[i]
             pointer.next = ListNode(data)
             pointer = pointer.next
         return result.next
